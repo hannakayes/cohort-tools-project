95cf1e9fdacb228aef8f41e1baf3541f0b0766ca
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-07-15 15:01:44 +0200

    Server set up

--- server/app.js
+++ server/app.js
@@ -1,34 +1,48 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fs = require("fs");
+const path = require("path");
 const PORT = 5005;
 
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
-
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
+// INITIALIZE EXPRESS APP
 const app = express();
 
-
 // MIDDLEWARE
-// Research Team - Set up CORS middleware here:
-// ...
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
-app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+// ROUTES
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the Cohort Tools API. Please visit /docs for the API documentation."
+  );
 });
 
+app.get("/docs", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "docs.html"));
+});
+
+app.get("/api/cohorts", (req, res) => {
+  fs.readFile(path.join(__dirname, "cohorts.json"), "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to load cohorts data" });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get("/api/students", (req, res) => {
+  fs.readFile(path.join(__dirname, "students.json"), "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to load students data" });
+    }
+    res.json(JSON.parse(data));
+  });
+});
 
 // START SERVER
 app.listen(PORT, () => {
